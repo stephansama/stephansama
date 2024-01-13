27ec757cdcae6842f7bac731f3d606a25c33165e
Stephan Randle <stephanrandle@hotmail.com>
2024-01-12 21:29:33 -0500

    updated brew install list

--- scripts/build/brew.sh
+++ scripts/build/brew.sh
@@ -22,10 +22,8 @@ brew install vim
 brew install tty-clock
 
 brew install --cask vlc
-brew install --cask gimp
 brew install --cask keka
 brew install --cask alfred
-brew install --cask firefox
 brew install --cask rectangle
 brew install --cask balenaetcher
 brew install --cask transmission
@@ -41,20 +39,31 @@ brew install --cask plex
 brewMessage 'Battery Related'
 brew install --cask aldente
 
+# Gaming
+brewMessage 'Gaming'
+brew install --cask steam
+brew install --cask battle-net
+
 # Internet
 brewMessage 'Internet'
 brew install --cask discord
+brew install --cask zoom
 brew install --cask megasync
 brew install --cask expressvpn
+brew install --cask firefox
 brew install --cask google-chrome
 
 # Hobbies
 brewMessage 'Hobby'
+brew install --cask notion
+brew install --cask gimp
+brew install --cask inkscape
 brew install --cask ultimaker-cura
 brew install --cask ableton-live-suite
 
 # Developer Tools
 brewMessage 'Developer'
+brew install nvm
 brew install --cask docker
 brew install --cask visual-studio-code
 
