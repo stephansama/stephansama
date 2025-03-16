9581cc67f451e4bf5ddc6e2d4311a620a0ea5ffb
stephansama <stephanrandle@hotmail.com>
2025-03-16 07:51:16 -0400

    chore: added early escape to fzf-projects

--- .config/scripts/fzf-projects.sh
+++ .config/scripts/fzf-projects.sh
@@ -3,9 +3,13 @@
 query=$(
 	find "$CODE" -type d -mindepth 1 -maxdepth 1 | sed "s@$CODE/@@g" | fzf \
 		--ansi \
-		--preview="lsd $CODE/{}"
+		--preview="lsd -1 $CODE/{}"
 )
+
+if [[ -z $query ]]; then
+	return
+fi
 
 cd "$CODE/$query" || exit
 
-nvim .
+e
