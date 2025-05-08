0712e12e0e6c1395d74fc0d608215a8123de9a9c
stephansama <stephanrandle@hotmail.com>
2025-05-07 21:46:31 -0400

    chore: removed catppuccin fzf theme

--- .config/fzf/mocha.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# https://github.com/catppuccin/fzf/blob/main/themes/catppuccin-fzf-mocha.sh
-export FZF_DEFAULT_OPTS=" \
---color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
---color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
---color=selected-bg:#45475A \
---color=border:#313244,label:#CDD6F4 \
---tmux=center"
