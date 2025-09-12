1128039a74f37d45dffd920785f683b2bf7720b4
stephansama <stephanrandle@hotmail.com>
2025-09-12 17:43:46 -0400

    chore: added submodule init deinit script

---
+++ submodule.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# select a submodule to interact with
+module=$(
+	git submodule foreach '' |
+		sed 's/Entering\ //g' |
+		tr -d "'" |
+		fzf
+)
+
+if [ -z "$module" ]; then
+	echo "No module selected"
+	exit 1
+fi
+
+action=$(printf "init\ndeinit" | fzf)
+
+if [ -z "$action" ]; then
+	echo "No action selected"
+	exit 1
+fi
+
+if [ "$action" = "init" ]; then
+	git submodule update --init "$module"
+else
+	git submodule deinit -f "$module"
+fi
